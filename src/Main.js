69a639b21e3b8c9b58d9dbfb8777a4615c97efb8
Enis Zekiqi <99254753+EnisZekiqi@users.noreply.github.com>
2024-05-31 23:33:42 +0200

    added responsive bottom-navigation

--- src/Main.js
+++ src/Main.js
@@ -51,7 +51,7 @@ const Main = () => {
   const avatarURL = Cookies.get('avatar');
   const { darkMode } = useDarkMode()
 const [fileURL, setFileURL] = useState('');
-
+const bg =darkMode ? "bg-rose" :"bg-pink"
 useEffect(() => {
   const savedFileURL = localStorage.getItem('selectedImageURL');
   if (savedFileURL) {
@@ -184,7 +184,14 @@ useEffect(() => {
       </div>
      <div className="mt-8 container mx-auto px-4 ">
      {xs && 
+     <div>
+      <div className={`hidden md:block ${bg}`}>
       <MainIntro/>
+      </div>
+      <div className={`block md:hidden${bg}`}>
+      <MainIntroSmallDisplay/>
+      </div>
+     </div>
      }
      </div>
       <div className="container mt-24 mx-auto px-4">
@@ -1264,7 +1271,7 @@ const togglePlatinum =()=>{
             </div>
      </div>
             <div className="flex mt-2 gap-2 items-center justify-center">
-              <button onClick={ToggleNotes}  className={`${buttonSwitch} p-2.5 px-12 md:p-2.5 text-center`}> <SendIcon/> Post</button>
+              <button onClick={ToggleNotes}  className={`${buttonSwitch} p-2.5  md:p-2.5 text-center`}> <SendIcon/> Post</button>
             </div>
       </div>
       <div className="empty"></div>
@@ -1451,5 +1458,302 @@ const togglePlatinum =()=>{
   )
  }
 
+
+const MainIntroSmallDisplay = ()=>{
+  
+  const { darkMode } = useDarkMode()
+  const theme = darkMode ? "backgroundIntro2":"backgroundIntro"
+  const themeText = darkMode ? "opacity-85":" opacity-75"
+  const borderTheme = darkMode ? "image-wrapper2" :"image-wrapper"
+  const gradient = darkMode 
+  ? 'linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 100%)' 
+  : 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 100%)' ;
+
+  const border = darkMode ? "border-rose" :" border-pink"
+  const bg =darkMode ? "bg-rose" :"bg-pink"
+  const inputSwitch = darkMode ? "input-wrapper2":"input-wrapper3"
+  const buttonSwitch =darkMode ? "btnsign":"btnsign3"
+  const buttonSwitch2 =darkMode ? "btnsaved":"btnsaved2"
+  const llojetsupl = darkMode ? "lloji-supleme":"lloji-supleme2"
+  ////////// color changes for dark/light mode ///////////
+
+    const [fillMessage,setFillMessage]=useState(false)
+    const [filledNotes,setFilledNotes]=useState(false)
+    const [maxFilled,setMaxFilled]=useState(false)
+    const [open1, setOpen1] = useState(false);
+const handleClose1 = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen1(false);
+  };
+  
+  const [fileURL, setFileURL] = useState('');
+
+  useEffect(() => {
+    const savedFileURL = localStorage.getItem('selectedImageURL');
+    if (savedFileURL) {
+      setFileURL(savedFileURL); // Set the file URL state with the saved image URL
+    }
+  }, []); ///////////// localstorage for avatar 
+
+
+  
+  useEffect(() => {
+    const savedNotes = Cookies.get('notes');
+    if (savedNotes) {
+      setNotes(savedNotes.split('\n')); // Split saved notes by newline character
+    }
+  }, []);
+
+  const [note, setNote] = useState('');
+  const [notes, setNotes] = useState([]);
+  const [displayedNotes, setDisplayedNotes] = useState(['']);
+ 
+
+  const handleNotesChange = (e)=>{
+    setNote(e.target.value)
+  }
+
+
+  const ToggleNotes = () => {
+    if (note.trim() === '') {
+      setFillMessage(true);
+      setOpen1(true);
+      setTimeout(() => {
+        setFillMessage(false);
+      }, 3000);
+    } else if (notes.length >= 3) {
+      setMaxFilled(true);
+      setOpen1(true);
+      setTimeout(() => {
+        setMaxFilled(false);
+      }, 3000);
+    } else {
+      const newNotes = [...notes, note];
+      setNotes(newNotes); // Update notes array
+      setFilledNotes(true); // Show success filled message
+      setOpen1(true);
+      Cookies.set('notes', newNotes.join('\n'), { expires: 365 }); // Join notes array with newline character
+      setTimeout(() => {
+        setFilledNotes(false);
+        setNote(''); // Clear the input field
+      }, 3000);
+    }
+  };
+
+  const [selectedNoteIndex, setSelectedNoteIndex] = useState(null);
+
+  const handleSelectNote = (index) => {
+    setSelectedNoteIndex(index);
+  };
+
+  const handleClearNote = () => {
+    if (selectedNoteIndex !== null) {
+      const newNotes = notes.filter((_, index) => index !== selectedNoteIndex);
+      setNotes(newNotes);
+      setSelectedNoteIndex(null);
+      Cookies.set('notes', newNotes.join('\n'), { expires: 365 });
+    }
+  };
+ 
+
+  const [news,setNews]=useState(false)
+
+  const toggleNews=()=>{
+    setNews(prevMode =>!prevMode)
+    setArchive(false)
+    setSaved(false)
+    setShowNotes(false)
+  }
+
+  //////////////// news //////////////////
+  const [showNotes,setShowNotes]=useState(false)
+
+  const toggleShowingNotes =()=>{
+    setShowNotes(prevMode =>!prevMode)
+    setNews(false)
+    setArchive(false)
+    setSaved(false)
+  }
+
+  ////////////showNotes/////////////
+
+  const [archive,setArchive]=useState(false)
+
+  const toggleArchive =()=>{
+    setArchive(prevMode=>!prevMode)
+    setNews(false)
+    setSaved(false)
+    setShowNotes(false)
+  }
+
+  ////////////archive //////////////////
+  const [saved,setSaved]=useState(false)
+
+  const toggleSaved =()=>{
+    setSaved(prevMode =>!prevMode)
+    setShowNotes(false)
+    setArchive(false)
+    setNews(false)
+  }
+////////////saved///////////////
+
+const [masstech,setMasstech]=useState(true)
+
+const toggleMasstech =()=>{
+  setMasstech(true)
+  setVapor(false)
+  setPlatinum(false)
+  setCell(false)
+}
+
+///////////masstech///////////
+
+const  [vapor,setVapor]=useState(false)
+
+const toggleVaport =()=>{
+  setVapor(true)
+  setMasstech(false)
+  setPlatinum(false)
+  setCell(false)
+}
+
+const [cell,setCell]=useState(false)
+
+const toggleCell=()=>{
+  setCell(true)
+  setVapor(false)
+  setMasstech(false)
+  setPlatinum(false)
+}
+
+const [platinum,setPlatinum]=useState(false)
+
+const togglePlatinum =()=>{
+  setPlatinum(true)
+  setVapor(false)
+  setMasstech(false)
+  setCell(false)
+}
+
+const toggleAllOff =()=>{
+  setArchive(false)
+  setShowNotes(false)
+  setSaved(false)
+  setNews(false)
+}
+
+
+  return(
+    <div className="relative min-h-screen">
+      <div className="maincontentsmallscreen">
+        <p>12312</p>
+      </div>
+        <div className={`fixed bottom-0 left-0 w-full p-4 ${bg}}`}
+        style={{backgroundColor:darkMode ? "#FAFBF9":"#050604"}}
+        >
+          <div className="empty"
+          style={{backgroundColor:darkMode ? "#FAFBF9":"#050604"}}
+          ></div>
+           <div className="empty"
+          style={{backgroundColor:darkMode ? "#FAFBF9":"#050604"}}
+          ></div>
+      <div className="flex justify-around items-center">
+        {/* News Feed */}
+        <div onClick={toggleNews} className="showNews flex flex-col items-center cursor-pointer">
+          <FeedIcon />
+          <h3 className="font-semibold text-sm mt-2">What is new</h3>
+        </div>
+
+        {/* Notes */}
+        <div onClick={toggleShowingNotes} className="flex flex-col items-center cursor-pointer">
+          <NewspaperIcon />
+          <h3 className="font-semibold text-sm mt-2">Notes</h3>
+        </div>
+
+        {/* Archive */}
+        <div onClick={toggleArchive} className="flex flex-col items-center cursor-pointer">
+          <ArchiveIcon />
+          <h3 className="font-semibold text-sm mt-2">Archive</h3>
+        </div>
+
+        {/* Saved */}
+        <div onClick={toggleSaved} className="flex flex-col items-center cursor-pointer">
+          <BookmarkBorderIcon />
+          <h3 className="font-semibold text-sm mt-2">Saved</h3>
+        </div>
+      </div>
+
+      {/* Drawer Content */}
+      <div onClick={toggleAllOff} className={`drawer-content ${news ? 'show' : ''}`}
+      style={{backgroundColor:darkMode ? "#050604":"#FAFBF9"}}
+      >
+         <div className="flex items-center justify-center"
+         style={{backgroundColor:darkMode ? "#FAFBF9":"#050604"}}
+         >
+       <div onClick={toggleAllOff}  className="px-2 pb-2 w-12 rounded-2xl mb-2" style={{backgroundColor:"#525252"}}/>
+       </div>
+        <motion.div variants={wrapperVariants} className="flex flex-col items-center gap-4 "
+         style={{backgroundColor:darkMode ? "#FAFBF9":"#050604"}}
+        >
+          <p className="font-light text-center text-sm mb-2 mt-2">Check news from our platform</p>
+          <motion.div variants={itemVariants} className={`flex pt-2 pl-2  pb-2 pr-2 ${bg} items-center rounded-xl gap-2`}>
+            <svg style={{ fill: darkMode ? "#FAFBF9" : "#050604" }} viewBox="0 0 24 24" fill="#FAFBF9" height="25px" width="35px">
+              <path d="M20 10c2 3-3 12-5 12s-2-1-3-1-1 1-3 1-7-9-5-12 5-3 7-2V5C5.38 8.07 4.11 3.78 4.11 3.78S6.77.19 11 5V3h2v5c2-1 5-1 7 2z" />
+            </svg>
+            <p style={{ color: darkMode ? "#FAFBF9" : "#050604" }} className="font-normal text-sm md:text-md text-start">New Healthy Diets added</p>
+          </motion.div>
+          <div className={`flex pt-2 pl-2 pr-2 pb-2 ${bg} items-center rounded-xl gap-2`}>
+            <svg style={{ fill: darkMode ? "#FAFBF9" : "#050604" }} viewBox="0 0 512 512" fill="#FAFBF9" height="25px" width="35px">
+              <path d="M480 448h-12a4 4 0 01-4-4V273.51a4 4 0 00-5.24-3.86 104.92 104.92 0 01-28.32 4.78c-1.18 0-2.3.05-3.4.05a108.22 108.22 0 01-52.85-13.64 8.23 8.23 0 00-8 0 108.18 108.18 0 01-52.84 13.64 106.11 106.11 0 01-52.46-13.79 8.21 8.21 0 00-8.09 0 108.14 108.14 0 01-53.16 13.8 106.19 106.19 0 01-52.77-14 8.25 8.25 0 00-8.16 0 106.19 106.19 0 01-52.77 14c-1.09 0-2.19 0-3.37-.05h-.06a104.91 104.91 0 01-29.28-5.09 4 4 0 00-5.23 3.8V444a4 4 0 01-4 4H32.5c-8.64 0-16.1 6.64-16.48 15.28A16 16 0 0032 480h447.5c8.64 0 16.1-6.64 16.48-15.28A16 16 0 00480 448zm-256-68a4 4 0 01-4 4h-88a4 4 0 01-4-4v-64a12 12 0 0112-12h72a12 12 0 0112 12zm156 68h-72a4 4 0 01-4-4V316a12 12 0 0112-12h56a12 12 0 0112 12v128a4 4 0 01-4 4zM492.57 170.28l-42.92-98.49C438.41 47.62 412.74 32 384.25 32H127.7c-28.49 0-54.16 15.62-65.4 39.79l-42.92 98.49c-9 19.41 2.89 39.34 2.9 39.35l.28.45c.49.78 1.36 2 1.89 2.78.05.06.09.13.14.2l5 6.05a7.45 7.45 0 00.6.65l5 4.83.42.36a69.65 69.65 0 009.39 6.78v.05a74 74 0 0036 10.67h2.47a76.08 76.08 0 0051.89-20.31l.33-.31a7.94 7.94 0 0110.89 0l.33.31a77.3 77.3 0 00104.46 0 8 8 0 0110.87 0 77.31 77.31 0 00104.21.23 7.88 7.88 0 0110.71 0 76.81 76.81 0 0052.31 20.08h2.49a71.35 71.35 0 0035-10.7c.95-.57 1.86-1.17 2.78-1.77A71.33 71.33 0 00488 212.17l1.74-2.63q.26-.4.48-.84c1.66-3.38 10.56-20.76 2.35-38.42z" />
+            </svg>
+            <p style={{ color: darkMode ? "#FAFBF9" : "#050604" }} className="font-normal text-sm md:text-md text-start">New Supplements</p>
+          </div>
+          <div className={`flex pt-2 pl-2 pr-2 pb-2 ${bg} items-center rounded-xl gap-2`}>
+            <FiInfo style={{ color: darkMode ? "#FAFBF9" : "#050604", width: '35px', height: '25px' }} />
+            <p style={{ color: darkMode ? "#FAFBF9" : "#050604" }} className="font-normal text-sm md:text-md">Information Edited</p>
+          </div>
+        </motion.div>
+      </div>
+
+      {/* Notes Drawer */}
+      <div onClick={toggleAllOff} className={`drawer-content ${showNotes ? 'show' : ''} `}
+       style={{backgroundColor:darkMode ? "#FAFBF9":"#050604"}}
+      >
+       <div className="flex items-center justify-center">
+       <div onClick={toggleAllOff}  className="px-2 pb-2 w-12 rounded-2xl " style={{backgroundColor:"#525252"}}/>
+       </div>
+        <p className="font-light text-center text-sm mb-2 ">Click on notes if you want to remove them</p>
+        <div className="flex flex-col gap-4 items-center">
+        {notes.map((note, index) => (
+          <p style={{ color: darkMode ? "#FAFBF9" : "#050604", cursor: 'pointer', backgroundColor: selectedNoteIndex === index ? 'rgba(128, 128, 128,0.9)' : '', transition: 'all 1s ease' }} className={`pt-2 pl-2 pb-2 pr-2 w-2/3 md:w-full ${bg} ${selectedNoteIndex === index ? 'bg-pink' : ''} items-center md:items-start rounded-xl`} key={index} onClick={() => handleSelectNote(index)}>{note}</p>
+        ))}
+        </div>
+        <div  className="flex justify-center items-center w-full mt-8">
+          <button onClick={handleClearNote} className={`${buttonSwitch} p-2.5 text-center`}><DeleteIcon /> Clear</button>
+        </div>
+      </div>
+
+      {/* Archive Drawer */}
+      <div onClick={toggleAllOff} className={`drawer-content ${archive ? 'show' : ''}`}>
+      <div className="flex items-center justify-center">
+       <div onClick={toggleAllOff}  className="px-2 pb-2 w-12 rounded-2xl " style={{backgroundColor:"#525252"}}/>
+       </div>
+        <h1>1</h1>
+      </div>
+
+      {/* Saved Drawer */}
+      <div onClick={toggleAllOff} className={`drawer-content ${saved ? 'show' : ''}`}>
+      <div className="flex items-center justify-center">
+       <div onClick={toggleAllOff}  className="px-2 pb-2 w-12 rounded-2xl " style={{backgroundColor:"#525252"}}/>
+       </div>
+        <h1>1</h1>
+      </div>
+    </div>
+    </div>
+  )
+}
 
 export default Main;
